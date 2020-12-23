41547548ace6516e8e509457e44cb428fc90f05d
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-12-23 10:44:10 +0100

    Gradle (#23)
    * gradle bump
    
    * julevask av gradle
    
    * bump shadow
    
    * ktlint
    
    * mindre warning logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,68 +1,38 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-group = "no.nav.syfo"
-version = "1.0.0"
-
+val ktorVersion = "1.5.0"
+val kotlinVersion = "1.4.21"
 val coroutinesVersion = "1.3.3"
 val jacksonVersion = "2.11.2"
 val kafkaEmbeddedVersion = "2.4.0"
 val postgresEmbeddedVersion = "0.13.3"
 val kluentVersion = "1.49"
-val ktorVersion = "1.3.2"
 val logbackVersion = "1.2.3"
 val logstashEncoderVersion = "5.1"
 val mockkVersion = "1.10.0"
 val nimbusdsVersion = "7.5.1"
 val prometheusVersion = "0.6.0"
 val smCommonVersion = "1.c22544d"
-val spekVersion = "2.0.9"
 val testContainerKafkaVersion = "1.15.0-rc2"
 val kafkaVersion = "2.6.0"
-
 val postgresVersion = "42.2.15"
 val flywayVersion = "6.5.4"
 val hikariVersion = "3.4.5"
-
-val ktlint by configurations.creating
-
-val outputDir = "${project.buildDir}/reports/ktlint/"
-val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
-
-tasks.withType<Jar> {
-    manifest.attributes["Main-Class"] = "no.nav.helse.flex.BootstrapKt"
-}
-
-plugins {
-    id("org.jmailen.kotlinter") version "2.1.1"
-    kotlin("jvm") version "1.3.70"
-    id("com.diffplug.gradle.spotless") version "3.23.1"
-    id("com.github.johnrengelman.shadow") version "4.0.4"
-    id("com.github.ben-manes.versions") version "0.29.0"
-    jacoco
-}
-
-buildscript {
-    dependencies {
-        classpath("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
-        classpath("org.glassfish.jaxb:jaxb-runtime:2.4.0-b180830.0438")
-        classpath("com.sun.activation:javax.activation:1.2.0")
-    }
-}
+val spekVersion = "2.0.9"
 
 val githubUser: String by project
 val githubPassword: String by project
 
+plugins {
+    application
+    kotlin("jvm") version "1.4.21"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+group = "no.nav.syfo"
+version = "1.0.0"
+
 repositories {
-    mavenCentral()
     jcenter()
-    maven(url = "https://dl.bintray.com/kotlin/ktor")
-    maven(url = "https://dl.bintray.com/spekframework/spek-dev")
-    maven(url = "https://packages.confluent.io/maven/")
-    maven(url = "https://kotlin.bintray.com/kotlinx")
-    maven(url = "https://jitpack.io")
+    maven { url = uri("https://packages.confluent.io/maven/") }
     maven {
         url = uri("https://maven.pkg.github.com/navikt/syfosm-common")
         credentials {
@@ -72,10 +42,23 @@ repositories {
     }
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.4.1")
+    }
+}
 
-    ktlint("com.pinterest:ktlint:0.37.2")
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-jackson:$ktorVersion")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:$coroutinesVersion")
     implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
@@ -106,12 +89,12 @@ dependencies {
 
     testImplementation("org.amshove.kluent:kluent:$kluentVersion")
     testImplementation("io.mockk:mockk:$mockkVersion")
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion")
     testImplementation("com.nimbusds:nimbus-jose-jwt:$nimbusdsVersion")
     testImplementation("org.testcontainers:kafka:$testContainerKafkaVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "org.eclipse.jetty")
     }
+
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion") {
         exclude(group = "org.jetbrains.kotlin")
     }
@@ -121,75 +104,22 @@ dependencies {
 
     testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedVersion")
     testImplementation("com.opentable.components:otj-pg-embedded:$postgresEmbeddedVersion")
+
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
 }
 
-val ktlintCheck by tasks.creating(JavaExec::class) {
-    inputs.files(inputFiles)
-    outputs.dir(outputDir)
-
-    description = "Check Kotlin code style."
-    classpath = ktlint
-    main = "com.pinterest.ktlint.Main"
-    args = listOf("src/**/*.kt")
-}
-
-val ktlintFormat by tasks.creating(JavaExec::class) {
-    inputs.files(inputFiles)
-    outputs.dir(outputDir)
-
-    description = "Fix Kotlin code style deviations."
-    classpath = ktlint
-    main = "com.pinterest.ktlint.Main"
-    args = listOf("-F", "src/**/*.kt")
-}
-
-tasks.jacocoTestReport {
-    reports {
-        xml.isEnabled = true
-        html.isEnabled = true
-    }
+application {
+    mainClassName = "no.nav.helse.flex.BootstrapKt"
 }
 
 tasks {
-
-    create("printVersion") {
-        println(project.version)
-    }
-
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "12"
-    }
-
-    withType<JacocoReport> {
-        classDirectories.setFrom(
-            sourceSets.main.get().output.asFileTree.matching {
-                exclude()
-            }
-        )
-    }
-    withType<ShadowJar> {
-        transform(ServiceFileTransformer::class.java) {
-            setPath("META-INF/cxf")
-            include("bus-extensions.txt")
-        }
-    }
-
     withType<Test> {
         useJUnitPlatform {
             includeEngines("spek2")
         }
         testLogging.showStandardStreams = true
     }
-
-    "check" {
-        dependsOn("formatKotlin")
+    named<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileTestKotlin") {
+        kotlinOptions.jvmTarget = "14"
     }
-}
-
-tasks.named("dependencyUpdates", DependencyUpdatesTask::class.java).configure {
-    // optional parameters
-    checkForGradleUpdate = true
-    outputFormatter = "json"
-    outputDir = "build/dependencyUpdates"
-    reportfileName = "report"
 }
