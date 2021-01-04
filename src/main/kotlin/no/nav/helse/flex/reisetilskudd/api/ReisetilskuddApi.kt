cd9db56706247bc4a11989a2f711af37d2275e9b
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-01-04 17:02:31 +0100

    Endre API på kvittering

--- src/main/kotlin/no/nav/helse/flex/reisetilskudd/api/ReisetilskuddApi.kt
+++ src/main/kotlin/no/nav/helse/flex/reisetilskudd/api/ReisetilskuddApi.kt
@@ -15,6 +15,7 @@ import no.nav.helse.flex.reisetilskudd.api.utils.toTextContent
 import no.nav.helse.flex.reisetilskudd.domain.Kvittering
 import no.nav.helse.flex.reisetilskudd.domain.Reisetilskudd
 import no.nav.helse.flex.reisetilskudd.domain.ReisetilskuddStatus
+import no.nav.helse.flex.reisetilskudd.domain.ReisetilskuddStatus.*
 import no.nav.helse.flex.reisetilskudd.domain.Svar
 
 fun Route.setupReisetilskuddApi(reisetilskuddService: ReisetilskuddService) {
@@ -35,6 +36,28 @@ fun Route.setupReisetilskuddApi(reisetilskuddService: ReisetilskuddService) {
         body(reisetilskudd)
     }
 
+    @ContextDsl
+    suspend fun Reisetilskudd.sjekkStatus(
+        statusList: List<ReisetilskuddStatus>,
+        call: ApplicationCall,
+        body: suspend Reisetilskudd.() -> Unit
+    ) {
+        if (statusList.contains(this.status)) {
+            body(this)
+        } else {
+            call.respond(Respons("Operasjonen støttes ikke på søknad med status $status").toTextContent(HttpStatusCode.BadRequest))
+        }
+    }
+
+    @ContextDsl
+    suspend fun Reisetilskudd.sjekkStatus(
+        status: ReisetilskuddStatus,
+        call: ApplicationCall,
+        body: suspend Reisetilskudd.() -> Unit
+    ) {
+        return sjekkStatus(listOf(status), call, body)
+    }
+
     route("/api/v1") {
         get("/reisetilskudd") {
             val fnr = call.fnr()
@@ -51,84 +74,78 @@ fun Route.setupReisetilskuddApi(reisetilskuddService: ReisetilskuddService) {
         put("/reisetilskudd/{reisetilskuddId}") {
 
             reisetilskudd {
-                val svarJson = call.receive<Svar>()
-                var reisetilskudd = this
-                if (svarJson.går != null) {
-                    reisetilskudd = reisetilskudd.copy(går = svarJson.går)
-                }
-                if (svarJson.sykler != null) {
-                    reisetilskudd = reisetilskudd.copy(sykler = svarJson.sykler)
-                }
-                if (svarJson.utbetalingTilArbeidsgiver != null) {
-                    reisetilskudd =
-                        reisetilskudd.copy(utbetalingTilArbeidsgiver = svarJson.utbetalingTilArbeidsgiver)
-                }
-                if (svarJson.egenBil != null) {
-                    reisetilskudd = reisetilskudd.copy(egenBil = svarJson.egenBil)
-                }
-                if (svarJson.kollektivtransport != null) {
-                    reisetilskudd = reisetilskudd.copy(kollektivtransport = svarJson.kollektivtransport)
-                }
-                val oppdatert = reisetilskuddService.oppdaterReisetilskudd(reisetilskudd)
+                sjekkStatus(ÅPEN, call) {
+                    val svar = call.receive<Svar>()
+                    var reisetilskudd = this
+                    if (svar.går != null) {
+                        reisetilskudd = reisetilskudd.copy(går = svar.går)
+                    }
+                    if (svar.sykler != null) {
+                        reisetilskudd = reisetilskudd.copy(sykler = svar.sykler)
+                    }
+                    if (svar.utbetalingTilArbeidsgiver != null) {
+                        reisetilskudd =
+                            reisetilskudd.copy(utbetalingTilArbeidsgiver = svar.utbetalingTilArbeidsgiver)
+                    }
+                    if (svar.egenBil != null) {
+                        reisetilskudd = reisetilskudd.copy(egenBil = svar.egenBil)
+                    }
+                    if (svar.kollektivtransport != null) {
+                        reisetilskudd = reisetilskudd.copy(kollektivtransport = svar.kollektivtransport)
+                    }
+                    val oppdatert = reisetilskuddService.oppdaterReisetilskudd(reisetilskudd)
 
-                call.respond(oppdatert)
+                    call.respond(oppdatert)
+                }
             }
         }
 
         post("/reisetilskudd/{reisetilskuddId}/send") {
             reisetilskudd {
-                if (status != ReisetilskuddStatus.ÅPEN) {
-                    call.respond(Respons("Kan ikke sende en søknad med status $status").toTextContent(HttpStatusCode.Forbidden))
-                    return@reisetilskudd
+                sjekkStatus(ÅPEN, call) {
+                    reisetilskuddService.sendReisetilskudd(fnr, reisetilskuddId)
+                    call.respond(Respons("Reisetilskudd $reisetilskuddId har blitt sendt").toTextContent())
                 }
-                reisetilskuddService.sendReisetilskudd(fnr, reisetilskuddId)
-                call.respond(Respons("Reisetilskudd $reisetilskuddId har blitt sendt").toTextContent())
-            }
-        }
-
-        post("/kvittering") {
-            val fnr = call.fnr()
-
-            val kvitteringJson = call.receive<Kvittering>()
-            if (reisetilskuddService.eierReisetilskudd(fnr, kvitteringJson.reisetilskuddId)) {
-                reisetilskuddService.lagreKvittering(kvitteringJson)
-                call.respond(Respons("${kvitteringJson.kvitteringId} ble lagret i databasen").toTextContent())
-            } else {
-                call.respond(Respons("Bruker eier ikke søknaden").toTextContent(HttpStatusCode.Forbidden))
             }
         }
 
         post("/reisetilskudd/{reisetilskuddId}/avbryt") {
             reisetilskudd {
-                if (!(status == ReisetilskuddStatus.ÅPEN || status == ReisetilskuddStatus.FREMTIDIG)) {
-                    call.respond(Respons("Kan ikke avbryte en søknad med status $status").toTextContent(HttpStatusCode.Forbidden))
-                    return@reisetilskudd
+                sjekkStatus(listOf(ÅPEN, FREMTIDIG), call) {
+                    reisetilskuddService.avbrytReisetilskudd(fnr, reisetilskuddId)
+                    call.respond(Respons("Reisetilskudd $reisetilskuddId har blitt avbrutt").toTextContent())
                 }
-                reisetilskuddService.avbrytReisetilskudd(fnr, reisetilskuddId)
-                call.respond(Respons("Reisetilskudd $reisetilskuddId har blitt avbrutt").toTextContent())
             }
         }
 
         post("/reisetilskudd/{reisetilskuddId}/gjenapne") {
             reisetilskudd {
-                if (status != ReisetilskuddStatus.AVBRUTT) {
-                    call.respond(Respons("Kan ikke gjenåpne en søknad med status $status").toTextContent(HttpStatusCode.Forbidden))
-                    return@reisetilskudd
+                sjekkStatus(AVBRUTT, call) {
+                    reisetilskuddService.gjenapneReisetilskudd(fnr, reisetilskuddId)
+                    call.respond(Respons("Reisetilskudd $reisetilskuddId har blitt gjenåpnet").toTextContent())
                 }
-                reisetilskuddService.gjenapneReisetilskudd(fnr, reisetilskuddId)
-                call.respond(Respons("Reisetilskudd $reisetilskuddId har blitt gjenåpnet").toTextContent())
             }
         }
 
-        delete("/kvittering/{kvitteringId}") {
-            val fnr = call.fnr()
-            val kvitteringId = call.parameters["kvitteringId"]!!
+        post("/reisetilskudd/{reisetilskuddId}/kvittering") {
+            reisetilskudd {
+                sjekkStatus(ÅPEN, call) {
+                    val kvittering = call.receive<Kvittering>()
+                    reisetilskuddService.lagreKvittering(kvittering, reisetilskuddId)
+                    call.respond(Respons("Kvittering ${kvittering.kvitteringId} ble lagret").toTextContent())
+                }
+            }
+        }
 
-            if (reisetilskuddService.eierKvittering(fnr, kvitteringId)) {
-                reisetilskuddService.slettKvittering(kvitteringId)
-                call.respond(Respons("kvitteringId $kvitteringId ble slettet fra databasen").toTextContent())
-            } else {
-                call.respond(Respons("Bruker eier ikke kvitteringen").toTextContent(HttpStatusCode.Forbidden))
+        delete("/reisetilskudd/{reisetilskuddId}/kvittering/{kvitteringId}") {
+            reisetilskudd {
+                reisetilskudd {
+                    sjekkStatus(ÅPEN, call) {
+                        val kvitteringId = call.parameters["kvitteringId"]!!
+                        reisetilskuddService.slettKvittering(kvitteringId = kvitteringId, reisetilskuddId = reisetilskuddId)
+                        call.respond(Respons("kvitteringId $kvitteringId ble slettet").toTextContent())
+                    }
+                }
             }
         }
     }
