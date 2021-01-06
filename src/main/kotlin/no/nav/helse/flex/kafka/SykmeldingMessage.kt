c402333355dfb38dca5a3f28ffc7919a0e047c55
kulsveen <bjorn.inge.kulsveen@nav.no>
2021-01-06 10:58:43 +0100

    Deler opp lange sykmeldingsperioder

--- src/main/kotlin/no/nav/helse/flex/kafka/SykmeldingMessage.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/SykmeldingMessage.kt
@@ -1,11 +1,12 @@
 package no.nav.helse.flex.kafka
 
-import no.nav.helse.flex.reisetilskudd.domain.Reisetilskudd
-import no.nav.helse.flex.reisetilskudd.util.reisetilskuddStatus
 import no.nav.syfo.model.sykmelding.kafka.EnkelSykmelding
+import no.nav.syfo.model.sykmelding.model.SykmeldingsperiodeDTO
 import no.nav.syfo.model.sykmeldingstatus.KafkaMetadataDTO
 import no.nav.syfo.model.sykmeldingstatus.SykmeldingStatusKafkaEventDTO
-import java.util.UUID
+import java.time.temporal.ChronoUnit
+import kotlin.math.ceil
+import kotlin.math.floor
 
 data class SykmeldingMessage(
     val sykmelding: EnkelSykmelding,
@@ -13,20 +14,36 @@ data class SykmeldingMessage(
     val event: SykmeldingStatusKafkaEventDTO
 )
 
-fun SykmeldingMessage.toReisetilskuddDTO(): List<Reisetilskudd> =
+fun SykmeldingMessage.reisetilskuddPerioder(): List<SykmeldingsperiodeDTO> =
     this.sykmelding.sykmeldingsperioder.filter {
         it.reisetilskudd
-    }.map {
-        Reisetilskudd(
-            status = reisetilskuddStatus(it.fom, it.tom),
-            oppfølgende = false,
-            reisetilskuddId = UUID.nameUUIDFromBytes("${this.sykmelding.id}-${it.fom}-${it.tom}".toByteArray())
-                .toString(),
-            sykmeldingId = this.sykmelding.id,
-            fnr = this.kafkaMetadata.fnr,
-            fom = it.fom,
-            tom = it.tom,
-            orgNavn = this.event.arbeidsgiver?.orgNavn,
-            orgNummer = this.event.arbeidsgiver?.orgnummer
-        )
+        // TODO: Finnes det flere perioder som skal med, eller noen perioder som ikke skal med?
     }
+
+fun List<SykmeldingsperiodeDTO>.splittLangeSykmeldingperioder(): List<SykmeldingsperiodeDTO> {
+    val soknadsPerioder: MutableList<SykmeldingsperiodeDTO> = mutableListOf()
+
+    // Hvis det er flere perioder så kan disse få forskjellige lengder, men ingen går over 31 dager
+    this.forEach { periode ->
+        val lengdePaaPeriode = ChronoUnit.DAYS.between(periode.fom, periode.tom) + 1
+        val antallDeler = ceil(lengdePaaPeriode / 31.0)
+        val grunnlengde = floor(lengdePaaPeriode / antallDeler)
+        var rest = lengdePaaPeriode % grunnlengde
+
+        var soknadFOM = periode.fom
+
+        var i = 0
+        while (i < antallDeler) {
+            val lengde = grunnlengde.toInt() + if (rest-- > 0) 1 else 0
+            val delperiode = periode.copy(fom = soknadFOM, tom = soknadFOM.plusDays((lengde - 1).toLong()))
+            soknadsPerioder.add(delperiode)
+            soknadFOM = delperiode.tom.plusDays(1)
+            i++
+        }
+    }
+
+    return soknadsPerioder
+}
+
+fun List<SykmeldingsperiodeDTO>.tidligstePeriodeFoerst(): List<SykmeldingsperiodeDTO> =
+    this.sortedBy { it.fom }
