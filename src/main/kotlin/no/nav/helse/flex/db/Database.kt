af8da6402eac0ec142e98e250a30cea95f39c436
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-02-04 14:49:08 +0100

    ingen bruk av connection

--- src/main/kotlin/no/nav/helse/flex/db/Database.kt
+++ src/main/kotlin/no/nav/helse/flex/db/Database.kt
@@ -4,6 +4,7 @@ import no.nav.helse.flex.domain.Kvittering
 import no.nav.helse.flex.domain.Reisetilskudd
 import no.nav.helse.flex.domain.ReisetilskuddStatus
 import no.nav.helse.flex.domain.Transportmiddel
+import no.nav.helse.flex.reisetilskudd.reisetilskuddStatus
 import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
@@ -16,17 +17,16 @@ import java.sql.Timestamp
 import java.time.Instant
 import java.time.LocalDate
 import java.time.OffsetDateTime
+import java.util.*
 import javax.sql.DataSource
 
 @Service
 @Transactional
 @Repository
 class Database(
-    private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate,
-    dataSource: DataSource
+    private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate
 ) {
 
-    val connection: Connection = dataSource.connection // TODO slett
 
     fun hentReisetilskuddene(fnr: String): List<Reisetilskudd> {
         val reisetilskudd = namedParameterJdbcTemplate.query(
@@ -83,6 +83,122 @@ class Database(
         )
 
         return hentReisetilskudd(reisetilskuddId)
+    }
+
+    fun oppdaterReisetilskudd(reisetilskudd: Reisetilskudd): Reisetilskudd {
+        val now = Instant.now()
+
+        namedParameterJdbcTemplate.update(
+            """
+              UPDATE reisetilskudd
+            SET (utbetaling_til_arbeidsgiver, gar, sykler, egen_bil, kollektivtransport, endret) = 
+                (:1, :2, :3, :4, :5, :6)                                
+            WHERE reisetilskudd_id = :7
+        """,
+            MapSqlParameterSource()
+                .addValue("1", reisetilskudd.utbetalingTilArbeidsgiver.toInt())
+                .addValue("2", reisetilskudd.går.toInt())
+                .addValue("3", reisetilskudd.sykler.toInt())
+                .addValue("4", reisetilskudd.egenBil)
+                .addValue("5", reisetilskudd.kollektivtransport)
+                .addValue("6", Timestamp.from(now))
+                .addValue("7", reisetilskudd.reisetilskuddId)
+        )
+
+        return hentReisetilskudd(reisetilskudd.reisetilskuddId)
+    }
+
+    fun gjenapneReisetilskudd(fnr: String, reisetilskuddId: String): Reisetilskudd {
+
+        val reisetilskudd = hentReisetilskudd(reisetilskuddId)
+
+        val status = reisetilskuddStatus(reisetilskudd.fom, reisetilskudd.tom)
+
+        namedParameterJdbcTemplate.update(
+            """
+              UPDATE reisetilskudd 
+           SET (status, avbrutt) = (:1, :2)
+           WHERE reisetilskudd_id = :3
+           AND fnr = :4
+           AND sendt is null
+           AND status = 'AVBRUTT'
+        """,
+            MapSqlParameterSource()
+                .addValue("1", status.name)
+                .addValue("2", null)
+                .addValue("3", reisetilskuddId)
+                .addValue("4", fnr)
+        )
+        return this.hentReisetilskudd(reisetilskuddId)
+    }
+
+    fun åpneReisetilskudd(id: String) {
+
+        namedParameterJdbcTemplate.update(
+            """
+            UPDATE reisetilskudd 
+            SET status = :1 
+            WHERE reisetilskudd_id = :2 
+            AND status = :3
+        """,
+            MapSqlParameterSource()
+                .addValue("1", ReisetilskuddStatus.ÅPEN.name)
+                .addValue("2", id)
+                .addValue("3", ReisetilskuddStatus.FREMTIDIG.name)
+        )
+    }
+
+    fun sendbarReisetilskudd(id: String) {
+        namedParameterJdbcTemplate.update(
+            """
+            UPDATE reisetilskudd 
+            SET status = :1 
+            WHERE reisetilskudd_id = :2 
+            AND status = :3
+        """,
+            MapSqlParameterSource()
+                .addValue("1", ReisetilskuddStatus.SENDBAR.name)
+                .addValue("2", id)
+                .addValue("3", ReisetilskuddStatus.ÅPEN.name)
+        )
+    }
+
+    fun lagreKvittering(kvittering: Kvittering, reisetilskuddId: String): Kvittering {
+        val now = Instant.now()
+        val id = UUID.randomUUID().toString()
+
+        namedParameterJdbcTemplate.update(
+            """
+              INSERT INTO kvitteringer
+                (kvittering_id, reisetilskudd_id, navn, belop, dato_for_reise, blob_id, transportmiddel, storrelse, opprettet)
+                VALUES(:1, :2, :3, :4, :5, :6, :7, :8, :9)
+        """,
+            MapSqlParameterSource()
+                .addValue("1", id)
+                .addValue("2", reisetilskuddId)
+                .addValue("3", kvittering.navn)
+                .addValue("4", kvittering.belop)
+                .addValue("5", Date.valueOf(kvittering.datoForReise))
+                .addValue("6", kvittering.blobId)
+                .addValue("7", kvittering.transportmiddel.name)
+                .addValue("8", kvittering.storrelse)
+                .addValue("9", Timestamp.from(now))
+        )
+
+        return kvittering.copy(kvitteringId = id)
+    }
+
+    fun slettKvittering(kvitteringId: String, reisetilskuddId: String): Int {
+        return namedParameterJdbcTemplate.update(
+            """
+                   DELETE FROM kvitteringer
+            WHERE kvittering_id = :1
+            AND reisetilskudd_id = :2
+        """,
+            MapSqlParameterSource()
+                .addValue("1", kvitteringId)
+                .addValue("2", reisetilskuddId)
+        )
     }
 
     fun hentReisetilskudd(reisetilskuddId: String): Reisetilskudd {
@@ -151,6 +267,42 @@ class Database(
         )
     }
 
+    fun finnReisetilskuddSomSkalBliSendbar(now: LocalDate): List<String> {
+        return namedParameterJdbcTemplate.query(
+            """
+              select * FROM reisetilskudd
+            WHERE status = :1 
+            AND tom < :2
+        """,
+            MapSqlParameterSource()
+                .addValue("1", ReisetilskuddStatus.ÅPEN.name)
+                .addValue("2", Date.valueOf(now)),
+            reisetilskuddIdRowMapper()
+        )
+    }
+
+    fun finnReisetilskuddSomSkalÅpnes(now: LocalDate): List<String> {
+        return namedParameterJdbcTemplate.query(
+            """
+               select * FROM reisetilskudd
+            WHERE status = :1 
+            AND fom <= :2
+        """,
+            MapSqlParameterSource()
+                .addValue("1", ReisetilskuddStatus.FREMTIDIG.name)
+                .addValue("2", Date.valueOf(now)),
+            reisetilskuddIdRowMapper()
+        )
+    }
+
+    private fun reisetilskuddIdRowMapper(): RowMapper<String> {
+        return RowMapper { resultSet, _ ->
+            with(resultSet) {
+                getString("reisetilskudd_id")
+            }
+        }
+    }
+
     private fun reisetilskuddRowMapper(): RowMapper<Reisetilskudd> {
         return RowMapper { resultSet, _ ->
             with(resultSet) {
@@ -192,5 +344,29 @@ class Database(
                 )
             }
         }
+    }
+}
+
+private fun Boolean?.toInt(): Int {
+    return when {
+        this == true -> 1
+        this == false -> -1
+        else -> 0
+    }
+}
+
+private fun Int.toOptionalBoolean(): Boolean? {
+    return when {
+        this == 1 -> true
+        this == -1 -> false
+        else -> null
+    }
+}
+
+private fun Int.toBoolean(): Boolean {
+    return when {
+        this == 1 -> true
+        this == -1 -> false
+        else -> throw IllegalArgumentException("$this må være -1 eller 1")
     }
 }
